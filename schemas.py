0bc7b2df22baa20f340eeb08b349aee68abe6439
Raphael KRIEF <kriefraphael88@gmail.com>
2020-10-08 11:14:58 +0200

    add desc

--- schemas.py
+++ schemas.py
@@ -4,7 +4,7 @@ from models import Product
 class ProductSchema(ma.Schema):
     class Meta:
         model = Product
-        fields = ('id', 'name') # These are the fields we want in the JSON!
+        fields = ('id', 'name', 'description') # These are the fields we want in the JSON!
 
 one_product_schema = ProductSchema()
 many_product_schema = ProductSchema(many=True)
